1281d16d82b272f72f7c04296ac8641512f52655
E. Almqvist <elalmqvist@gmail.com>
2022-08-27 22:13:09 +0200

    CLI flags

--- main.go
+++ main.go
@@ -6,14 +6,15 @@ import (
 	"os"
 	"strings"
 	"time"
+	"flag"
 
 	"github.com/Jeffail/tunny"
 	"github.com/xrjr/mcutils/pkg/ping"
 )
 
-func getIps() []string {
+func getIps(ips_file string) []string {
 	// Read ips from file possible_minecraft_servers.txt
-	file, err := os.Open("real_mc_servers.txt")
+	file, err := os.Open(ips_file)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -26,45 +27,60 @@ func getIps() []string {
 	return ips
 }
 
-func main() {
-	// // Loop over ips and ping them
-	// for _, ip := range getIps() {
-	// 	properties, _, err := ping.Ping(ip, 25565)
-	// 	if err != nil {
-	// 		fmt.Println(err)
-	// 	}
-	// 	fmt.Println(properties.Infos().Players)
-	// }
+// CLI flags
+var help = flag.Bool("help", false, "Show help")
 
-	// Loop over ips and ping them in a goroutine
-	// for _, ip := range getIps() {
-	// 	go func(ip string) {
-	// 		properties, _, err := ping.Ping(ip, 25565)
-	// 		if err != nil {
-	// 			return
-	// 		}
-	// 		fmt.Println(properties.Infos().Players)
-	// 	}(ip)
-	// }
+var outfileFlag = "output.txt"
+var ipsfileFlag = "ips.txt"
+var numThreadsFlag = 1000
+var portFlag = 25565
+var startAtFlag = ""
+
+func main() {
+	// Binds the flags
+	flag.StringVar(&outfileFlag, "o", "output.txt", "Output file (where all the data goes).")
+	flag.StringVar(&ipsfileFlag, "i", "", "File containing all the IPv4s separated by linebreaks (input).")
+	flag.StringVar(&startAtFlag, "r", "", "Start at this IPv4 (resume).")
+	flag.IntVar(&numThreadsFlag, "n", 1000, "Number of scan workers (threads).")
+	flag.IntVar(&portFlag, "p", 25565, "Server port to use.")
+
+	// Parse the flags
+	flag.Parse()
+
+	// Validate dumb shit
+	if ipsfileFlag == "" {
+		fmt.Println("You must provide a input file!\nExample: `$ mcmap -i list-of-servers.txt`\nWrite `$ mcmap -h` for help.")
+		os.Exit(1)
+	}
+
+	if numThreadsFlag <= 0 {
+		fmt.Println(fmt.Sprintf("Number of threads must be >= 1, must provide more threads! %d <= 0!\nWrite `$ mcmap -h` for help.", numThreadsFlag))
+		os.Exit(1)
+	}
+
+	if *help {
+		flag.Usage()
+		os.Exit(0)
+	}
 
 	// Loop over ips and batch them into an array of 10 ips, then ping them in a goroutine
-	allIps := getIps()
+	allIps := getIps(ipsfileFlag)
 
-	var startAt string = "218.148.136.167"
+	//var startAt string = "218.148.136.167"
 
-	file, err := os.OpenFile("server-map12.txt", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+	file, err := os.OpenFile(outfileFlag, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
 	if err != nil {
 		fmt.Println(err)
 	}
 
 	defer file.Close()
 
-	pool := tunny.NewFunc(1000, func(data interface{}) interface{} {
+	pool := tunny.NewFunc(numThreadsFlag, func(data interface{}) interface{} {
 		ip, ok := data.(string)
 		if !ok {
 			return nil
 		}
-		properties, ping, err := ping.Ping(ip, 25565)
+		properties, ping, err := ping.Ping(ip, portFlag)
 		if err != nil {
 			fmt.Println(err)
 			return nil
@@ -77,7 +93,7 @@ func main() {
 		}
 
 		formattedData := fmt.Sprintf(
-			"%s (ping: %d, players: %d, version: %s, modt: %s names: %s)\n",
+			"%s (ping: %d, players: %d, version: %s, motd: %s names: %s)\n",
 			ip, ping, properties.Infos().Players.Online, properties.Infos().Version.Name,
 			properties.Infos().Description, str,
 		)
@@ -89,13 +105,14 @@ func main() {
 	})
 
 	defer pool.Close()
-	shouldStart := true
+	shouldStart := startAtFlag == ""
 
 	for _, ip := range allIps {
-		if ip == startAt {
+		if ip == startAtFlag {
 			shouldStart = true
 			continue
 		}
+
 		if !shouldStart {
 			continue
 		}
@@ -108,6 +125,4 @@ func main() {
 	for pool.QueueLength() > 0 {
 		time.Sleep(time.Second)
 	}
-
-	// Write servers with players and is 1.19.X to file servers_with_players_and_is_1.19.X.txt
 }
